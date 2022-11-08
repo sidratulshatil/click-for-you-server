1d6ba37dacafce99e15d2bb102324f34b5884ead
sidratulshatil <105915771+sidratulshatil@users.noreply.github.com>
2022-11-08 15:41:55 +0600

    get id based service added

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const cors = require('cors')
 const app = express()
 const port = process.env.PORT || 5000
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 // Middleware
 app.use(cors())
 app.use(express.json())
@@ -18,12 +18,26 @@ async function run() {
     try {
         const serviceCollection = client.db('photography').collection('services')
 
+        app.get('/services3', async (req, res) => {
+            const query = {}
+            const cursor = serviceCollection.find(query)
+            const services = await cursor.limit(3).toArray()
+            res.send(services)
+
+        })
         app.get('/services', async (req, res) => {
             const query = {}
             const cursor = serviceCollection.find(query)
             const services = await cursor.toArray()
             res.send(services)
 
+        })
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: ObjectId(id) }
+            const service = await serviceCollection.findOne(query)
+
+            res.send(service)
         })
 
     }
